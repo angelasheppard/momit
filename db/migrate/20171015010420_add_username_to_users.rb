c90afae135b81ff25304a79a1fd124023df116a0
AShep <amshepp+cloud9@gmail.com>
2017-12-30 02:00:25 +0000

    Migration to add uniqueness constraint on username

--- db/migrate/20171015010420_add_username_to_users.rb
+++ db/migrate/20171015010420_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.1]
     def change
         add_column :users, :username, :string, limit: 30, null: false, unique: true
-        add_index :users, :username
+        add_index :users, :username, unique: true
     end
 end
