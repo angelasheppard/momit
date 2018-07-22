6d82c301f5baa467e49077410e03a9fa8f78f7d1
AShep <amshepp+cloud9@gmail.com>
2018-07-22 12:50:10 +0000

    Use Devise test helpers in controller specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,9 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # Use Devise test helpers in controller specs
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
