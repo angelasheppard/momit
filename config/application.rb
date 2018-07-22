6548dc409772ec163a84461d5de9eacce78ecd02
AShep <amshepp+cloud9@gmail.com>
2018-07-22 15:26:13 +0000

    Correct typo in rspec generator config

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,7 @@ module Momit
       g.test_framework :rspec,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
-	request_spects: false
+        routing_specs: false
     end
   end
 end
