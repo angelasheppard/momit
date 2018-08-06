25c8b4a63586366908899291de0938ff3885e230
AShep <amshepp+cloud9@gmail.com>
2018-08-06 18:49:53 +0000

    Customize Pundit unauthorized flash error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    private
+
+    def user_not_authorized(exception)
+        policy_name = exception.policy.class.to_s.underscore
+
+        flash[:error] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+        redirect_to(request.referrer || root_path)
+    end
+
     protected
 
     def configure_permitted_parameters
