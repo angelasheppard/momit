2cb45fab2fef1692121e3782e471c197e77dfe55
AShep <amshepp+cloud9@gmail.com>
2018-08-06 18:51:33 +0000

    Setup pundit application and user policies, spec for user policy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, except: [:create]
+    after_action :verify_authorized
 
     def index
         @title = "Users"
         @users = User.ordered_by_username
+        authorize current_user
+    end
+
+    def edit
     end
 
     private
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,44 +1,16 @@
 class ApplicationPolicy
-    attr_reader :user, :record
+    attr_reader :current_user, :record
 
-    def initialize(user, record)
-        @user = user
+    def initialize(current_user, record)
+        @current_user = current_user
         @record = record
     end
 
-    def index?
-        false
-    end
-
-    def show?
-        false
-    end
-
-    def create?
-        false
-    end
-
-    def new?
-        create?
-    end
-
-    def update?
-        false
-    end
-
-    def edit?
-        update?
-    end
-
-    def destroy?
-        false
-    end
-
     class Scope
-        attr_reader :user, :scope
+        attr_reader :current_user, :scope
 
-        def initialize(user, scope)
-            @user = user
+        def initialize(current_user, scope)
+            @current_user = current_user
             @scope = scope
         end
 
