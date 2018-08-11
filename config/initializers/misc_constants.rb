ec2dc9a9846a78ab5895f8f2574c4992f4169eb7
AShep <amshepp+cloud9@gmail.com>
2018-08-11 16:07:48 +0000

    Add notification email, long timestamp constants

--- config/initializers/misc_constants.rb
+++ config/initializers/misc_constants.rb
@@ -1,4 +1,11 @@
 SHORT_TIMESTAMP_FORMAT = "%-m/%-d/%y %H:%M"
 TIMESTAMP_FORMAT = "%m/%d/%Y %H:%M"
+LONG_TIMESTAMP_FORMAT = "%Y-%m-%d:%H:%M:%S"
+
+if Rails.env == 'production'
+    NOTIFICATION_EMAIL_TO = User.admins.pluck(:email)
+elsif Rails.env == 'development'
+    NOTIFICATION_EMAIL_TO = User.admins.first.email
+end
 
 WARCRAFT_LOGS_PATH = "https://www.warcraftlogs.com/guild/id/111533"
