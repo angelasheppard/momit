aba429e1366fd682fb53bdf34fe22788ccfae9f5
AShep <amshepp+cloud9@gmail.com>
2018-08-17 02:12:12 +0000

    Override devise registrations controller to add logging

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: {
       confirmations: 'users/confirmations',
+      registrations: 'users/registrations',
     },
     path_names: {
       sign_in: 'login',
