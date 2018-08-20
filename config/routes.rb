1212f97261267e0a7b9ae9121cd059414c6ce618
Melthery <washerdrier@gmail.com>
2018-08-20 23:59:39 +0000

    commenting stuff out to get dbsetup to run

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,23 @@ Rails.application.routes.draw do
   match '/code-of-conduct', to: 'static_pages#code_of_conduct', via: [:get]
   match '/guild-policies', to: 'static_pages#guild_policies', via: [:get]
 
-	match '/users', to: 'users#index', via: 'get'
+# 	match '/users', to: 'users#index', via: 'get'
 
-  devise_for :users,
-    controllers: {
-      confirmations: 'users/confirmations',
-      registrations: 'users/registrations',
-      sessions: 'users/sessions',
-      passwords: 'users/passwords',
-    },
-    path_names: {
-      sign_in: 'login',
-      sign_out: 'logout'
-    }
+#   devise_for :users,
+#     controllers: {
+#       confirmations: 'users/confirmations',
+#       registrations: 'users/registrations',
+#       sessions: 'users/sessions',
+#       passwords: 'users/passwords',
+#     },
+#     path_names: {
+#       sign_in: 'login',
+#       sign_out: 'logout'
+#     }
 
-  scope "/admin" do
-    resources :users
-  end
+#   scope "/admin" do
+#     resources :users
+#   end
 
   root 'static_pages#home'
 end
