6348e578d457be7fcf571646b1db3190e40c1763
AShep <amshepp+cloud9@gmail.com>
2018-09-05 17:30:36 +0000

    Guild policies page and css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,17 @@
 class StaticPagesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:home]
+    skip_before_action :authenticate_user!,
+        only: [ :home,
+                :code_of_conduct,
+                :guild_policies ]
 
     def home
     end
 
     def code_of_conduct
+        @title = 'Code of Conduct'
     end
 
     def guild_policies
+        @title = 'Guild Policies'
     end
 end
