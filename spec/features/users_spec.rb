60f5c3630d3e78b3561a6f89b8f1176ac0941b39
AShep <amshepp+cloud9@gmail.com>
2018-09-13 19:22:47 +0000

    User messageboard read access specs

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-    let(:admin_user) { FactoryBot.create(:user, :is_admin) }
-    before do
-        sign_in admin_user
-        visit root_path
-    end
-
     context "authorized users" do
+        let(:admin_user) { FactoryBot.create(:user, :is_admin) }
+        before do
+            sign_in admin_user
+            visit root_path
+        end
+
         scenario "successfully edit a user" do
             guest_user = FactoryBot.create(:user)
             click_link 'Admin'
@@ -55,6 +55,72 @@ RSpec.feature "Users", type: :feature do
             expect(current_path).to eq users_path
             expect(page).to have_content("User #{guest_user.username} has been deleted")
             expect(page).to_not have_link('Destroy', href: user_path(guest_user))
+        end
+    end
+
+    context "when logged in as a guest" do
+        scenario "only public messageboards are visible" do
+            guest_user = FactoryBot.create(:user)
+            sign_in guest_user
+            visit thredded_path
+
+            expect(page).to have_content('Public')
+            expect(page).to have_link('Recruitment', href: "/forum/recruitment")
+            expect(page).to_not have_link('Officers', href: '/forum/officers')
+        end
+    end
+    context "when logged in as an initiate" do
+        scenario "public and some momit messageboards are visible" do
+            initiate_user = FactoryBot.create(:user, :is_initiate)
+            sign_in initiate_user
+            visit thredded_path
+
+            expect(page).to have_content('Public')
+            expect(page).to have_selector('h3', text: 'MOMiT')
+            expect(page).to have_link('Recruitment', href: "/forum/recruitment")
+            expect(page).to have_link('Class & Strategy Discussion', href: '/forum/class-strategy-discussion')
+            expect(page).to_not have_link('Officers', href: '/forum/officers')
+            expect(page).to_not have_link('Recruit Voting', href: '/forum/recruit-voting')
+        end
+    end
+    context "when logged in as a member" do
+        scenario "public and some momit messageboards are visible" do
+            member_user = FactoryBot.create(:user, :is_member)
+            sign_in member_user
+            visit thredded_path
+
+            expect(page).to have_content('Public')
+            expect(page).to have_selector('h3', text: 'MOMiT')
+            expect(page).to have_link('Recruitment', href: "/forum/recruitment")
+            expect(page).to have_link('Class & Strategy Discussion', href: '/forum/class-strategy-discussion')
+            expect(page).to have_link('Recruit Voting', href: '/forum/recruit-voting')
+            expect(page).to_not have_link('Officers', href: '/forum/officers')
+        end
+    end
+    context "when logged in an as officer or admin" do
+        scenario "all messageboards are visible" do
+            officer_user = FactoryBot.create(:user, :is_officer)
+            sign_in officer_user
+            visit thredded_path
+
+            expect(page).to have_content('Public')
+            expect(page).to have_selector('h3', text: 'MOMiT')
+            expect(page).to have_link('Recruitment', href: "/forum/recruitment")
+            expect(page).to have_link('Class & Strategy Discussion', href: '/forum/class-strategy-discussion')
+            expect(page).to have_link('Recruit Voting', href: '/forum/recruit-voting')
+            expect(page).to have_link('Officers', href: '/forum/officers')
+
+            sign_out officer_user
+
+            admin_user = FactoryBot.create(:user, :is_admin)
+            sign_in admin_user
+
+            expect(page).to have_content('Public')
+            expect(page).to have_selector('h3', text: 'MOMiT')
+            expect(page).to have_link('Recruitment', href: "/forum/recruitment")
+            expect(page).to have_link('Class & Strategy Discussion', href: '/forum/class-strategy-discussion')
+            expect(page).to have_link('Recruit Voting', href: '/forum/recruit-voting')
+            expect(page).to have_link('Officers', href: '/forum/officers')
         end
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,9 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
+
+  # Load data from seeds file
+  config.before(:suite) do
+    require "#{Rails.root}/db/seeds.rb"
+  end
 end
