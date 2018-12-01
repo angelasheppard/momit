bd83fb12eb983067f6438db7cf0220cfd178384e
AShep <amshepp+cloud9@gmail.com>
2018-12-01 21:52:56 +0000

    Add specs for forum write access

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -59,22 +59,36 @@ RSpec.feature "Users", type: :feature do
     end
 
     context "when logged in as a guest" do
-        scenario "only public messageboards are visible" do
-            guest_user = FactoryBot.create(:user)
+        let(:guest_user) { create(:user) }
+        before do
             sign_in guest_user
             visit thredded_path
+        end
 
+        scenario "only public messageboards are visible" do
             expect(page).to have_content('Public')
             expect(page).to have_link('Recruitment', href: "/forum/recruitment")
             expect(page).to_not have_link('Officers', href: '/forum/officers')
         end
+
+        scenario "can write to visible messageboards" do
+            visit "/forum/#{guest_user.thredded_can_write_messageboards.map(&:name).first.parameterize}"
+            expect(page).to have_field('Start a New Topic')
+        end
+
+        scenario "cannot visit restricted messageboards" do
+            visit "/forum/recruit-voting"
+            expect(page).to have_content('not authorized')
+        end
     end
     context "when logged in as an initiate" do
-        scenario "public and some momit messageboards are visible" do
-            initiate_user = FactoryBot.create(:user, :is_initiate)
+        let(:initiate_user) { create(:user, :is_initiate) }
+        before do
             sign_in initiate_user
             visit thredded_path
+        end
 
+        scenario "public and some momit messageboards are visible" do
             expect(page).to have_content('Public')
             expect(page).to have_selector('h3', text: 'MOMiT')
             expect(page).to have_link('Recruitment', href: "/forum/recruitment")
@@ -82,13 +96,26 @@ RSpec.feature "Users", type: :feature do
             expect(page).to_not have_link('Officers', href: '/forum/officers')
             expect(page).to_not have_link('Recruit Voting', href: '/forum/recruit-voting')
         end
+
+        scenario "can write to visible messageboards" do
+            visit "/forum/#{initiate_user.thredded_can_write_messageboards.map(&:name).first.parameterize}"
+            expect(page).to have_field('Start a New Topic')
+        end
+
+        scenario "cannot visit restricted messageboards" do
+            visit "/forum/recruit-voting"
+            expect(page).to have_content('not authorized')
+        end
     end
+
     context "when logged in as a member" do
-        scenario "public and some momit messageboards are visible" do
-            member_user = FactoryBot.create(:user, :is_member)
+        let(:member_user) { create(:user, :is_member) }
+        before do
             sign_in member_user
             visit thredded_path
+        end
 
+        scenario "public and some momit messageboards are visible" do
             expect(page).to have_content('Public')
             expect(page).to have_selector('h3', text: 'MOMiT')
             expect(page).to have_link('Recruitment', href: "/forum/recruitment")
@@ -96,31 +123,56 @@ RSpec.feature "Users", type: :feature do
             expect(page).to have_link('Recruit Voting', href: '/forum/recruit-voting')
             expect(page).to_not have_link('Officers', href: '/forum/officers')
         end
+
+        scenario "can write to visible messageboards" do
+            visit "/forum/#{member_user.thredded_can_write_messageboards.map(&:name).first.parameterize}"
+            expect(page).to have_field('Start a New Topic')
+        end
+
+        scenario "cannot visit restricted messageboards" do
+            visit "/forum/officers"
+            expect(page).to have_content('not authorized')
+        end
     end
-    context "when logged in an as officer or admin" do
-        scenario "all messageboards are visible" do
-            officer_user = FactoryBot.create(:user, :is_officer)
+    context "when logged in an as officer" do
+        let(:officer_user) { create(:user, :is_officer) }
+        before do
             sign_in officer_user
             visit thredded_path
-
+        end
+        scenario "all messageboards are visible" do
             expect(page).to have_content('Public')
             expect(page).to have_selector('h3', text: 'MOMiT')
             expect(page).to have_link('Recruitment', href: "/forum/recruitment")
             expect(page).to have_link('Class & Strategy Discussion', href: '/forum/class-strategy-discussion')
             expect(page).to have_link('Recruit Voting', href: '/forum/recruit-voting')
             expect(page).to have_link('Officers', href: '/forum/officers')
+        end
 
-            sign_out officer_user
+        scenario "can write to visible messageboards" do
+            visit "/forum/#{officer_user.thredded_can_write_messageboards.map(&:name).first.parameterize}"
+            expect(page).to have_field('Start a New Topic')
+        end
+    end
 
-            admin_user = FactoryBot.create(:user, :is_admin)
+    context "when logged in as an admin" do
+        let(:admin_user) { create(:user, :is_admin) }
+        before do
             sign_in admin_user
-
+            visit thredded_path
+        end
+        scenario "all messageboards are visible" do
             expect(page).to have_content('Public')
             expect(page).to have_selector('h3', text: 'MOMiT')
             expect(page).to have_link('Recruitment', href: "/forum/recruitment")
             expect(page).to have_link('Class & Strategy Discussion', href: '/forum/class-strategy-discussion')
             expect(page).to have_link('Recruit Voting', href: '/forum/recruit-voting')
             expect(page).to have_link('Officers', href: '/forum/officers')
+        end
+
+        scenario "can write to visible messageboards" do
+            visit "/forum/#{admin_user.thredded_can_write_messageboards.map(&:name).first.parameterize}"
+            expect(page).to have_field('Start a New Topic')
         end
     end
 end
